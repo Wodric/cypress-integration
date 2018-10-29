0560704f3722753e1c8ccccceb3225b795ac52a1
Alexandre Lefevre <alefevre@smartwavesa.com>
2018-10-29 09:48:48 +0100

    Standardize my test

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -1,6 +1,5 @@
 describe('Test home page', function() {
     it('successfully loads example cypress home page', function() {
-      cy.visit('/')
-
+      cy.visit('https://example.cypress.io/')
     })
   })
